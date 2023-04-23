ba18297bcc1006adb92ad1f76dfa736de0c8e0d9
Simran Fairoz <74310809+Simran31-ps@users.noreply.github.com>
2023-04-24 02:36:49 +0530

    Update main.php

--- terraformers/applicationlayer/main.php
+++ terraformers/applicationlayer/main.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Doctor Patient</title>
-	<link rel="stylesheet"  href="main.css">
+	<link rel="stylesheet"  href="doctorpatient.css">
 	
 
 	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
